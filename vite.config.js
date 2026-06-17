23bd7bfd39423fceaf683766725e8a4f29f0687b
r.golovkov <r.golovkov@yadro.com>
2026-06-17 15:38:53 +0300

    upd2

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,9 +5,16 @@ const REPO_NAME = 'salary_calculator';
 
 export default defineConfig({
   plugins: [react()],
-    base: `/salary_calculator/`,
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+  base: `/${REPO_NAME}/`,
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+    strictPort: true,
+    allowedHosts: ['all'],
+    hmr: {
+      protocol: 'ws',
+      host: undefined,
+      port: 80,
+    },
   },
 })
