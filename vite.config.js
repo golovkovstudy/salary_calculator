beb640051ac9e4c1bffe2792204475d1418516e5
r.golovkov <r.golovkov@yadro.com>
2026-06-17 15:30:00 +0300

    add ci cfg

--- vite.config.js
+++ vite.config.js
@@ -6,25 +6,9 @@ const REPO_NAME = 'salary_calculator';
 
 export default defineConfig({
   plugins: [react()],
-    base: `/${REPO_NAME}/`,
+    base: `/salary_calculator/`,
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
   },
 })
-
-
-// export default defineConfig({
-//     plugins: [react()],
-//   server: {
-//     host: '0.0.0.0',
-//     port: 3327,
-//     strictPort: true,
-//     allowedHosts: ['all'], // Обязательно для Vite 4.5+ / 5.x
-//     hmr: {
-//       protocol: 'ws',
-//       host: undefined, // Автоматически подхватит IP браузера
-//       port: 3327,
-//     },
-//   },
-// })
